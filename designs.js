1c7a36295867ae997f11e32397905d35d42c7405
Felipe <felipeleon92@gmail.com>
2019-01-04 17:41:42 -0500

    added user ability to color cells
    created the addColorListener function in order

--- designs.js
+++ designs.js
@@ -7,22 +7,34 @@ const table = document.querySelector ("#pixelCanvas");
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  // Checks if there's an existing table, if so it deletes it
   if (table.firstChild) {
     table.removeChild(table.firstChild);
   };
   var grid = document.createElement("tbody");
+  // Loops over based off users input for height & width
   for (let r = 1;r <= height.value;r++) {
     var row = document.createElement('tr');
     grid.appendChild(row);
     for (let c = 1;c <= width.value;c++) {
-      var cell = document.createElement('td')
+      var cell = document.createElement('td');
       row.appendChild(cell);
     };
   table.appendChild(grid);
   };
 };
 
+function addColorListener() {
+  allCells = document.querySelectorAll("td");
+    for (let c = 0;c < allCells.length; c++) {
+      allCells[c].addEventListener("click", function(e) {
+        e.target.style.backgroundColor = color.value;
+      });
+    };
+};
+
 document.querySelector("#submit").addEventListener("click", function(e) {
   e.preventDefault();
   makeGrid();
+  addColorListener();
 });
